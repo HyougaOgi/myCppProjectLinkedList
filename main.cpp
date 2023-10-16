d6047caad40c414f94df994fc8c01211b24151e0
Hyoga Ogi <hyouga.0818@icloud.com>
2023-10-17 02:03:29 +0900

    First commit write push function

---
+++ main.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+
+class Node{
+public:
+    int data;
+    Node* next;
+    Node* prev;
+    Node(int data) {
+        this->data = data;
+        this->next = nullptr;
+        this->prev = nullptr;
+    }
+};
+
+
+class LinkedList{
+public:
+    Node* head;
+    Node* tail;
+    LinkedList(Node* node){
+        this->head = node;
+        this->tail = nullptr;
+    }
+    void push(int data){
+        if(this->head == nullptr){
+            this->head = new Node(data);
+        }
+        else if (this->tail == nullptr){
+            this->tail = new Node(data);
+            this->head->next = this->tail;
+            this->tail->prev = this->head;
+        }
+        else{
+            Node* node = new Node(data);
+            this->tail->next = node;
+            node->prev = this->tail;
+            this->tail = this->tail->next;
+        }
+    }
+    Node* pop(int index){
+
+        return new Node(0);
+    }
+    int getValue(int index){
+        return 0;
+    }
+};
+
+
+
+
+
+int main() {
+
+}
+
+
